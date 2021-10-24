ac32bcc2f28756952805faf45831b4e74c8f074f
Andrew Lau <alauwyk@gmail.com>
2021-10-23 19:23:39 -0700

    finished the exercise for getter and setter

--- 3. Getters and Setters/examples/PlayingWithJunit.java
+++ 3. Getters and Setters/examples/PlayingWithJunit.java
@@ -1,7 +1,9 @@
 package examples;
+import static junit.framework.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
 
 public class PlayingWithJunit {
 
